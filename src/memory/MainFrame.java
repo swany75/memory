3afb73a83b2006272f162316c90c13d056dd5782
swany75 <juandalmausantandreu@gmail.com>
2026-04-29 09:42:11 +0200

    Timer & Screen final touches

--- src/memory/MainFrame.java
+++ src/memory/MainFrame.java
@@ -69,6 +69,7 @@ public class MainFrame extends JFrame {
 
         add(topPanel(), BorderLayout.NORTH);
         add(centerPanel(), BorderLayout.CENTER);
+        
         add(timer, BorderLayout.SOUTH);
         setVisible(true);
     }
--- src/memory/Timer.java
+++ src/memory/Timer.java
@@ -11,27 +11,70 @@ package memory;
 
 import javax.swing.*;
 import java.awt.*;
+public class Timer extends JProgressBar {
 
-public class Timer extends JPanel {
-    
-    private JLabel labelTemps;
+    private javax.swing.Timer crono;
+    private int secTotals;
+    private int secRestants;
 
     public Timer() {
-        setLayout(new BorderLayout());
+        super();
+        setMinimum(0);
+        setMaximum(100);
+        setValue(0);
+        setStringPainted(true);
         setPreferredSize(new Dimension(0, 32));
-
-        labelTemps = new JLabel("--:--", SwingConstants.CENTER);
-        labelTemps.setFont(new Font("JetBrains Mono", Font.BOLD, 14));
-
-        add(labelTemps, BorderLayout.CENTER);
+        setFont(new Font("JetBrains Mono", Font.BOLD, 14));
+        setForeground(new Color(0x3C1A7B));
+        setBackground(new Color(0xDDCCFF));
     }
 
-    public void setTime(String time) {
-        labelTemps.setText(time);
+    // JProgressBar usa este método para saber qué texto pintar
+    @Override
+    public String getString() {
+        if (secTotals == 0) return "--:--";
+        int min = secRestants / 60;
+        int seg = secRestants % 60;
+        return String.format("%02d:%02d", min, seg);
+    }
+
+    public void prepararCountdown(int minuts) {
+        this.secTotals   = minuts * 60;
+        this.secRestants = secTotals;
+
+        setMaximum(secTotals);
+        setValue(0);
+        repaint();
+
+        crono = new javax.swing.Timer(1000, e -> {
+            secRestants--;
+            setValue(secTotals - secRestants);
+            repaint();                        
+
+            if (secRestants <= 0) {
+                stop();
+                JOptionPane.showMessageDialog(null, "GAME OVER!");
+            }
+        });
+    }
+
+    public void start() { 
+        if (crono != null) {
+            crono.start();
+        } 
+    }
+    
+    public void stop()  { 
+        if (crono != null) {
+            crono.stop();
+        }  
     }
 
     public void reset() {
-        labelTemps.setText("--:--");
+        stop();
+        secTotals   = 0;
+        secRestants = 0;
+        setValue(0);
+        repaint();
     }
-    
 }
